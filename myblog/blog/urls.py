f32e5475e14d8bb4c220553bcbadf064a9e5dc7e
jodie528 <43610109+jodie528@users.noreply.github.com>
2019-03-12 20:08:19 +0000

    modify2

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -9,4 +9,6 @@ from . import  views
 
 urlpatterns = [
     path('index/', views.index),
+    path('article/<article_id>', views.article_page, name='article_page'),
+    path('edit/', views.edit_page),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Django中所有的请求都用函数处理
 
 def index(request):
-    return render(request, 'blog/index.html')
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
+
+def edit_page(request):
+    return render(request, 'blog/edit_page.html')
